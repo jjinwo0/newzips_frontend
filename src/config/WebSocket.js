316227bafde59fd55438545cf9c830b335b773e2
jinwoo <zvzv9808@gmail.com>
2024-05-21 11:53:00 +0900

    fix: 실시간 채팅 버그 수정

--- src/config/WebSocket.js
+++ src/config/WebSocket.js
@@ -14,20 +14,35 @@ export default {
 
   // 방 입장 시 사용
   enterRoom(roomId, user) {
-    stompClient.send(`/app/room/${roomId}/entered`, JSON.stringify(user), {});
+    stompClient.send(`/pub/room/${roomId}/entered`, JSON.stringify(user), {});
   },
 
   // 메시지 전송 시 사용
-  sendMessage(roomId, message) {
-    stompClient.send(`/app/room/${roomId}`, JSON.stringify(message), {});
+  sendMessageToRoom(nickname, roomId, message) {
+    const chatMessage = JSON.stringify({
+      sender: nickname,
+      message: message,
+      timestamp: new Date().toISOString()
+    });
+    stompClient.send(`/pub/room/${roomId}`, chatMessage,{});
   },
 
   // 특정 방의 메시지를 구독하기 위한 메소드
   subscribeToRoom(roomId, callback) {
+    console.log(roomId)
     stompClient.subscribe(`/sub/room/${roomId}`, message => {
-      callback(JSON.parse(message.body));
+      console.log('Received message:', message.body); // 메시지 내용 로깅
+
+      try {
+        const parsedMessage = JSON.parse(message.body);
+        console.log('parsed message :: ', parsedMessage);
+        callback(parsedMessage);
+      } catch (error) {
+        console.error('Error parsing message:', error, message.body);
+      }
     });
   },
+  
 
   // 연결 끊기
   disconnect() {
